40423a2cab5269499f85532acf45ef077e30b197
Ryan Barlow <908032@swansea.ac.uk>
2022-11-04 17:31:22 +0000

    Relationships for user, updated primary key, and changed fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
+    
+    /**
+     * The Users's ID.
+     */
+    protected $uuid = 'uuid';
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +22,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'fname',
+        'lname',
+        'dob',
+        'mobile',
         'email',
         'password',
     ];
@@ -41,4 +48,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-}
+
+    /**
+     * Child Relationship: 
+     * User can have many posts.
+     * User can have many comments.
+     * User can have many replies.
+    */
+    public function userPosts() {
+        return $this->hasMany("App\Models\Post", "uuid");
+    }
+    public function userComments() {
+        return $this->hasMany("App\Models\Comment", "uuid");
+    }
+    public function userReplies() {
+        return $this->hasMany("App\Models\Reply", "uuid");
+    }
+};
