5f6c43c60a9d92478f34da0bc53393b26eb8173c
Ryan Barlow <908032@swansea.ac.uk>
2022-11-04 17:35:25 +0000

    Created file, added urid, fillable, relationships

---
+++ app/Models/Reply.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Reply extends Model
+{
+    use HasFactory;
+
+    /**
+     * The Replies Unique ID.
+     */
+    protected $urid = 'urid';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'upid',
+        'content',
+        'timestamp',
+        'upvotes',
+        'downvotes',
+    ];
+
+    /**
+     * Parent Relationship:
+     * Reply belongs to a user.
+     * Reply belongs to a comment.
+    */
+    public function user() {
+        //uuid = Unique User ID
+        return $this->belongsTo("App\Models\User", "uuid");
+    }
+    public function comment() {
+        //uuid = Unique Comment ID
+        return $this->belongsTo("App\Models\Reply", "ucid");
+    }
+}
