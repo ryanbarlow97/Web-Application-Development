648817d542c31d836e272ea291adc2d7e15d411f
Ryan Barlow <908032@swansea.ac.uk>
2022-11-04 18:30:57 +0000

    create migrations file for comments

--- database/migrations/2022_11_04_180807_create_comments_table.php
+++ database/migrations/2022_11_04_180807_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -19,8 +19,8 @@ return new class extends Migration
             $table->string('content');
             $table->integer('upvotes');
             $table->integer('downvotes');
-            $table->timestamps();
-
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
         });
     }
 
