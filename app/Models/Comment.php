a931d27fb214d03f4df4366364c2e0dfdba11476
Ryan Barlow <908032@swansea.ac.uk>
2022-11-04 17:32:35 +0000

    Created file, added ucid, fillable, relationships

---
+++ app/Models/Comment.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The Comments's Unique ID.
+     */
+    protected $ucid = 'ucid';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'upid',
+        'content',
+        'timestamp',
+        'upvotes',
+        'downvotes',
+    ];
+
+    /**
+     * Parent Relationship:
+     * Comment belongs to user.
+     * Comment belongs to post.
+    */
+    public function user() {
+        //uuid = Unique User ID
+        return $this->belongsTo("App\Models\User", "uuid");
+    }
+    public function post() {
+        //upid = Unique Post ID
+        return $this->belongsTo("App\Models\Post", "upid");
+    }
+
+    /**
+     * Child Relationship: 
+     * Comment has many replies.
+     */
+    public function replies() {
+        //uuid = Unique User ID
+        return $this->hasMany("App\Models\Reply", "uuid");
+    }
+}
