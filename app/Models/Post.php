aba9552c1f5d3e65d7bba596b231b6aa6616436b
Ryan Barlow <908032@swansea.ac.uk>
2022-11-04 17:32:06 +0000

    Created post.php, added upid, fillable data, and relationships

---
+++ app/Models/Post.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The Post's ID.
+     */
+    protected $upid = 'upid';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'uuid',
+        'content',
+        'timestamp',
+        'upvotes',
+        'downvotes',
+    ];
+
+    /**
+     * Parent Relationship: 
+     * Post belongs to user.
+    */
+    public function user() {
+        //uuid = Unique User ID
+        return $this->belongsTo("App\Models\User", "uuid");
+    }
+    
+    /**
+     * Child Relationship: 
+     * Post can have many comments.
+    */
+    public function comments() {
+        return $this->hasMany("App\Models\Comment", "upid");
+    }
+}
