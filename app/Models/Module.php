f9b8dfd0c629ffcf1f6449c230bd2995c78dbb74
Ryan Barlow <908032@swansea.ac.uk>
2022-11-05 14:32:11 +0000

    updated module

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Module extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'description', 'module_logo',
+    ];
+
+    /**
+    * A module area can have many posts.
+    */
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
+
+    /**
+    * A module area can have many comments.
+    */
+    public function comments() {
+        return $this->hasMany(Commment::class);
+    }
+
+    /**
+    * A module area can have many comments.
+    */
+    public function enrolledStudents() {
+        return $this->belongsToMany(User::class, 'enrolled');
+    }
+
 }
