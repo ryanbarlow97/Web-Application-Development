d92dbd97047088e0d94a22c054d886bec04e781b
Ryan Barlow <908032@swansea.ac.uk>
2022-11-05 16:10:33 +0000

    create userfactory with faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,12 +4,12 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
-{
+class UserFactory extends Factory {
     /**
      * Define the model's default state.
      *
@@ -17,24 +17,23 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+        $created = $this->faker->dateTimeBetween('2010-01-01', '2022-11-05')
+        ->format('Y/m/d');
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return [
+            //create fake user
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'date_of_birth' => $this->faker->dateTimeBetween('1970-01-01', '2003-12-31')
+                ->format('Y/m/d'),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'mobile' => $this->faker->phoneNumber(),
+            'password' => hash('sha512', $this->faker->name()),
+            'remember_token' => Str::random(10),
+            'created_at' => $created,
+            'updated_at' => $this->faker->dateTimeBetween($created, '2022-11-05')
+                ->format('Y/m/d'),
+        ];
     }
 }
