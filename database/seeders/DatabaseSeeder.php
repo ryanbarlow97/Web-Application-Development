748b16496c093d6559cc1ec841f6e6a043e0fc04
Ryan Barlow <908032@swansea.ac.uk>
2022-11-05 16:10:18 +0000

    call seeder class user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,8 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    public function run() {
+        //Call to User seeder
+        $this->call(UserSeeder::class);
     }
 }
