fa680f14289abbad3f666557c69e54ddd4d26c15
Ryan Barlow <908032@swansea.ac.uk>
2022-11-06 21:36:21 +0000

    added RNG profile picture name.png

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,7 @@ class UserFactory extends Factory {
             'email_verified_at' => now(),
             'mobile' => $this->faker->phoneNumber(),
             'password' => hash('sha512', $this->faker->name()),
+            'profile_picture' => Str::random(12) . '.png',
             'remember_token' => Str::random(12),
             'created_at' => $created,
             'updated_at' => $this->faker->dateTimeBetween($created, '2022-11-05')
