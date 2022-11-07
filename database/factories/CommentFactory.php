18b0aa0c2145a7ad537504333a2b1bade8a6e88b
Ryan Barlow <908032@swansea.ac.uk>
2022-11-07 04:25:35 +0000

    changed the way data is created

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Comment;
+use App\Models\User;
+use App\Models\Post;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
@@ -17,15 +19,21 @@ class CommentFactory extends Factory {
     public function definition() {
         $created = $this->faker->dateTimeBetween('2010-01-01', '2022-11-05')
         ->format('Y/m/d');
+        $updated = $this->faker->dateTimeBetween($created, '2022-11-05')
+        ->format('Y/m/d');
+
+        $first_post= Post::inRandomOrder()->first();
+        $first_user = $first_post->module->users->random()->users;
+
+
 
         return [
             //create fake posts inside module area
-            'user_id' => $this->faker->numberBetween(1,50),
-            'post_id' => $this->faker->numberBetween(1,50),
+            'user_id' => $first_user,
+            'post_id' => $first_post,
             'content' => $this->faker->text(),
             'created_at' => $created,
-            'updated_at' => $this->faker->dateTimeBetween($created, '2022-11-05')
-                ->format('Y/m/d'),
+            'updated_at' => $updated,
             ];
     }
 }
