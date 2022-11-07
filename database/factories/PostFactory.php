204dc8da5cf743494d251006f18c30b3fba005ed
Ryan Barlow <908032@swansea.ac.uk>
2022-11-07 04:26:59 +0000

    changed way posts are made

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Module;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,16 +19,20 @@ class PostFactory extends Factory {
     public function definition() {
         $created = $this->faker->dateTimeBetween('2010-01-01', '2022-11-05')
             ->format('Y/m/d');
+        $updated = $this->faker->dateTimeBetween($created, '2022-11-05')
+            ->format('Y/m/d');
+
+        $first_module = Module::inRandomOrder()->first();
+        $first_user = $first_module->users->random()->users;
 
         return [
             //create fake posts inside module area
-            'user_id' => $this->faker->unique()->numberBetween(1,50),
-            'module_id' => $this->faker->numberBetween(1,12),
+            'user_id' => $first_user,
+            'module_id' => $first_module,
             'content' => $this->faker->text(),
             'flair' => $this->faker->text(6),
             'created_at' => $created,
-            'updated_at' => $this->faker->dateTimeBetween($created, '2022-11-05')
-                ->format('Y/m/d'),
+            'updated_at' => $updated,
         ];
     }
 }
