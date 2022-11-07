8b052194c7432e5b05e3e92fd78f5b313330601f
Ryan Barlow <908032@swansea.ac.uk>
2022-11-07 04:27:57 +0000

    renamed to Likeable

--- database/seeders/LikeableSeeder.php
+++ database/seeders/LikeableSeeder.php
@@ -6,13 +6,12 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Likeable;
 
-class LikeablesSeeder extends Seeder {
+class LikeableSeeder extends Seeder {
     /**
      * Run the database seeds.
-     *
      * @return void
      */
-    public function run(){
-        Likeable::factory(25)->create();
+    public function run() {
+        Likeable::factory(100)->create();
     }
 }
