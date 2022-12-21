3c01ec48a7f7668836cb975770d9d704cca8a6a8
Ryan Barlow <908032@swansea.ac.uk>
2022-12-21 23:04:18 +0000

    add routes for profile/home/notifications

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', function () {
+    return view('profile');
+})->middleware(['auth', 'verified'])->name('profile');
+
+
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
+
+
+Route::get('/profile1', function () {
+    return view('profile1');
+})->middleware(['auth', 'verified'])->name('profile1');
+
+require __DIR__.'/auth.php';
