7382cd81afe0b0221df810142f1854f5f0e4a44c
Ryan Barlow <908032@swansea.ac.uk>
2022-12-22 00:27:41 +0000

    update navidation bar to represet new areas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,23 +3,17 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('profile') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
-                </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
                         {{ __('Home') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('profile1')" :active="request()->routeIs('profile1')">
-                        {{ __('Profile1') }}
+                    <x-nav-link :href="route('notifications')" :active="request()->routeIs('notifications')">
+                        {{ __('Notifications') }}
                     </x-nav-link>       
-                    <x-nav-link :href="route('profile')" :active="request()->routeIs('profile')">
-                        {{ __('Profile') }}
+                    <x-nav-link :href="route('users.show', ['id' => Auth::user()->id])" :active="request()->routeIs('users')">
+                        {{ __('My Profile') }}
                     </x-nav-link>                 
                 </div>
             </div>
@@ -78,14 +72,14 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('profile')" :active="request()->routeIs('home')">
+            <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
                 {{ __('Home') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('notifications')" :active="request()->routeIs('notifications')">
                 {{ __('Notifications') }}
             </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('profile')" :active="request()->routeIs('profile')">
-                {{ __('Profile') }}
+            <x-responsive-nav-link :href="route('users.show', ['id' => Auth::user()->id])" :active="request()->routeIs('users')">
+                {{ __('My Profile') }}
             </x-responsive-nav-link>
         </div>
        
