e172387210e7f07c111795aba92b38ea0f9e9dba
Ryan Barlow <908032@swansea.ac.uk>
2022-12-22 01:10:56 +0000

    changed css to space out posts

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,24 +1,23 @@
 <style>
-.comment-box {
-    background-color: lightblue;
-    padding: 10px;
-    margin: auto;
-    width: 100%;
+table {
+  margin: auto;
+  width: 45%;
 }
 
-table {
-  table-layout: fixed;
-  margin: auto;
-  border-collapse: collapse;
-  width: 60%;
-}
 td {
-  border: 1px solid #000;
-  width: 75px;
+  width: 50px;
 }
 td+td {
   width: fit-content;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.25);
+  padding: 5px;
+  margin: auto;
 }
+.gfg {
+    border-collapse:separate;
+    border-spacing:0 10px;
+}
+
 
 </style>
 
@@ -29,20 +28,20 @@ td+td {
         </h2>
     </x-slot>
     
+    
+        
+@foreach ($users->posts as $post)
+    <table class = "gfg">
+        <tr>
+            <td>
+                <a href="{{ $users->id}}"><img src="{{ $users->profile_picture}}" alt="HTML tutorial" style="width:42px;height:42px;"></a>
+            </td>
+            <td>
+                {{ $post->content}}
+            </td>
+        </tr>
     <table>
-        <br>
-        @foreach ($users->posts as $post)
-            <tr>
-                <td>
-                {{ $users->profile_picture}}
-                </td>
-                <td>
-                    <div class="comment-box">
-                       {{ $post->content}}
-                    </div>
-                </td>
-            </tr>
-        @endforeach
-    <table>
+@endforeach
+
     
 </x-app-layout>
