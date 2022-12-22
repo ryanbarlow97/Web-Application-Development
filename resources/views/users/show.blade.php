cbfe55b86212cd943d037f34ef0d0e0a031c8a87
Ryan Barlow <908032@swansea.ac.uk>
2022-12-22 02:55:54 +0000

    basic home page -> will show generic user profile

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,24 +1,10 @@
+
 <style>
-table {
+table.table2 {
   margin: auto;
-  width: 45%;
+  width: 50%;
+  text-align: center;
 }
-
-td {
-  width: 50px;
-}
-td+td {
-  width: fit-content;
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.25);
-  padding: 5px;
-  margin: auto;
-}
-.gfg {
-    border-collapse:separate;
-    border-spacing:0 10px;
-}
-
-
 </style>
 
 <x-app-layout>
@@ -26,22 +12,31 @@ td+td {
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ ucfirst(strtolower($users->first_name)) }} {{ ucfirst(strtolower($users->last_name)) }}
         </h2>
+        <table class = "table2">
+            <tr>
+                <td>
+                    <x-responsive-nav-link :href="route('users.posts', ['id' => $users->id])" :active="request()->routeIs('users.posts')">
+                        {{ __('Posts') }}
+                    </x-responsive-nav-link>
+                </td>
+                <td>
+                    <x-responsive-nav-link :href="route('users.comments', ['id' => $users->id])" :active="request()->routeIs('users.comments')">
+                        {{ __('Comments') }}
+                    </x-responsive-nav-link>
+                </td>
+                <td>
+                    <x-responsive-nav-link :href="route('users.liked_posts', ['id' => $users->id])" :active="request()->routeIs('users.liked_posts')">
+                        {{ __('Liked Posts') }}
+                    </x-responsive-nav-link>
+                </td>
+                <td>
+                    <x-responsive-nav-link :href="route('users.liked_comments', ['id' => $users->id])" :active="request()->routeIs('users.liked_comments')">
+                        {{ __('Liked Comments') }}
+                    </x-responsive-nav-link>
+                </td>
+            </div>
+            </div>
+            </tr>
+        </table>
     </x-slot>
-    
-    
-        
-@foreach ($users->posts as $post)
-    <table class = "gfg">
-        <tr>
-            <td>
-                <a href="{{ $users->id}}"><img src="{{ $users->profile_picture}}" alt="HTML tutorial" style="width:42px;height:42px;"></a>
-            </td>
-            <td>
-                {{ $post->content}}
-            </td>
-        </tr>
-    <table>
-@endforeach
-
-    
 </x-app-layout>
