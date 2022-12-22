4a71268341d9ff4594522a367c63469c82d86c52
Ryan Barlow <908032@swansea.ac.uk>
2022-12-22 00:27:04 +0000

    add ability to see posts and users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,29 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
+ 
+use App\Models\User;
+ 
 class UserController extends Controller
 {
-    //
+    /**
+     * Show the profile for a given user.
+     *
+     * @param  int  $id
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        return view('users.show', [
+            'users' => User::findOrFail($id)
+        ]);
+    }
+
+    public function posts($id)
+    {
+        return view('users.posts', [
+            'users' => Post::findOrFail($id)
+        ]);
+    }
+
 }
