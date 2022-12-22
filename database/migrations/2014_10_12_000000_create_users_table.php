8393bf918e145848753132745852fb85de74537c
Ryan Barlow <908032@swansea.ac.uk>
2022-12-22 00:26:51 +0000

    readd profile picture

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             //string to support +44
             #$table->string('mobile')->unique();
-            #$table->string('profile_picture');
+            $table->string('profile_picture')->nullable();
             #$table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
