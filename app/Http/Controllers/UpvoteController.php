c94e46d2259342357a4bc2f4693dbe8891161daa
Ryan Barlow <908032@swansea.ac.uk>
2022-12-23 01:36:28 +0000

    reworked to merge upvoteComment and upvotePost, now returns a json instead of a redirect

--- app/Http/Controllers/UpvoteController.php
+++ app/Http/Controllers/UpvoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Comment;
 use App\Models\Likeable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
@@ -12,49 +13,50 @@ class UpvoteController extends Controller
 {
     public function upvote(Request $request)
     {
-        // Validate the request and extract the post ID and user ID
-        $validatedData = $request->validate([
-            'post_id' => 'required|exists:posts,id',
-            'user_id' => 'required|exists:users,id',
-        ]);
-        
-        $postId = $validatedData['post_id'];
-        $userId = $validatedData['user_id'];
-
-        // Check if the user has already upvoted the post
-        $post = Post::findOrFail($postId);
-        $like = Likeable::where([
-            ['likeable_id', $postId],
-            ['likeable_type', 'App\Models\Post'],
-            ['user_id', $userId],
-        ])->first();
-
-        if ($like) {
-            // If the user has already upvoted the post, delete the like
-            Likeable::where([
-                ['likeable_id', $postId],
-                ['likeable_type', 'App\Models\Post'],
-                ['user_id', $userId],
-            ])->delete();
-            
-        } else {
-            // Create a new upvote record
-            Likeable::create([
-                'user_id' => $userId,
-                'likeable_id' => $postId,
-                'likeable_type' => 'App\Models\Post',
-            ]);
-        }
-
-        // Get the current number of likes for the post
-        $likeCount = $post->likeables()->count();
-
-
-        // Redirect back to the page
-        return Redirect::back()
-        ->with('success', 'Upvote successful!')
-        ->with('likeCount', $likeCount);
-    }
+      // Validate the request data
+      $request->validate([
+        'likeable_id' => 'required|integer',
+        'likeable_type' => 'required|string',
+        'user_id' => 'required|integer',
+      ]);
+    
+      // Find the likeable object and user
+      $likeable = $request->likeable_type::findOrFail($request->likeable_id);
+      $user = \App\Models\User::findOrFail($request->user_id);
+    
+      // Check if the user has already upvoted the likeable object
+      $like = \App\Models\Likeable::where([
+        ['likeable_id', $likeable->id],
+        ['likeable_type', $request->likeable_type],
+        ['user_id', $user->id],
+      ])->first();
+    
+      if ($like) {
+        // If the user has already upvoted the likeable object, remove the upvote
+        $like = \App\Models\Likeable::where([
+            ['likeable_id', $likeable->id],
+            ['likeable_type', $request->likeable_type],
+            ['user_id', $user->id],
+          ])->delete();
+        $likes = count($likeable->likeables);
+        $upvoted = false;
+      } else {
+        // If the user has not upvoted the likeable object, add an upvote
+        $like = new \App\Models\Likeable();
+        $like->likeable_id = $likeable->id;
+        $like->likeable_type = $request->likeable_type;
+        $like->user_id = $user->id;
+        $like->save();
+        $likes = count($likeable->likeables);
+        $upvoted = true;
+      }
+    
+      // Return a JSON response with the updated number of likes
+      return response()->json([
+        'likes' => $likes,
+        'upvoted' => $upvoted,
+      ]);
+    }    
 }
         
         
