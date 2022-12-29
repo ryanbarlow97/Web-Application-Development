8a4179b43428992fed8b01a469ffece802ab9a85
Ryan Barlow <908032@swansea.ac.uk>
2022-12-29 21:13:40 +0000

    fixed mistake when changing delete post/delete comment

--- resources/views/livewire/comment-delete.blade.php
+++ resources/views/livewire/comment-delete.blade.php
@@ -1,3 +1,15 @@
 <div>
-  <button wire:click="deletePost">Delete</button>
+  <!-- Use a button element to display the delete button -->
+<button class="text-xs" wire:click="deleteComment">Delete</button>
+
+<!-- Add a Livewire event listener to handle the deletion of comments -->
+<script>
+  window.livewire.on('commentDeleted', commentId => {
+    // Find the list-group-item element that contains the data-comment-id attribute with the matching value
+    const commentElement = document.querySelector(`[data-comment-id="${commentId}"]`);
+
+    // Delete the comment element from the DOM
+    commentElement.remove();
+  });
+</script>
 </div>
