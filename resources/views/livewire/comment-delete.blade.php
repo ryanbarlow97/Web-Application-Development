66ce472ecf549105a53cfad721f3b0a990455c39
Ryan Barlow <908032@swansea.ac.uk>
2022-12-29 17:40:12 +0000

    emit "deletePost" when clicking delete

--- resources/views/livewire/comment-delete.blade.php
+++ resources/views/livewire/comment-delete.blade.php
@@ -1,15 +1,3 @@
 <div>
-  <!-- Use a button element to display the delete button -->
-<button wire:click="deleteComment">Delete</button>
-
-<!-- Add a Livewire event listener to handle the deletion of comments -->
-<script>
-  window.livewire.on('commentDeleted', commentId => {
-    // Find the list-group-item element that contains the data-comment-id attribute with the matching value
-    const commentElement = document.querySelector(`[data-comment-id="${commentId}"]`);
-
-    // Delete the comment element from the DOM
-    commentElement.remove();
-  });
-</script>
+  <button wire:click="deletePost">Delete</button>
 </div>
