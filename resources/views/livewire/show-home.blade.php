017a5c82ea365255251fa0f6c7128485984590ea
Ryan Barlow <908032@swansea.ac.uk>
2022-12-29 21:14:55 +0000

    revammped home page to look like comment page

--- resources/views/livewire/show-home.blade.php
+++ resources/views/livewire/show-home.blade.php
@@ -10,21 +10,27 @@
   </div>
 </x-slot>
 
-<table class="table-auto bg-white shadow-md rounded-md mt-6 mx-auto" wire:poll.2s>
-  @foreach (\App\Models\Post::all()->sortBy('created_at')->reverse() as $posts)
-  <tr class="odd:bg-white even:bg-slate-50 border-b border-gray-200">
-          <td class="px-6 py-4 text-left">
-            <a href="{{route('profile', ['user_name' => $posts->user->user_name])}}">
-              <!-- Use Tailwind classes to style the profile picture -->
-              <img class="w-12 h-12 rounded-full" src="{{ $posts->user->profile_picture}}">
-            </a>
-          </td>
-          <td class="px-6 py-4 text-left max-w-3xl text-base leading-5 font-medium text-gray-900">
-            <h4 class="text-left text-xs">{{ $posts->user->first_name }} {{ $posts->user->last_name }} wrote a <a class="text-blue-700" href="{{route('post', ['id' => $posts->id])}}"> post</a> {{ $posts->created_at->diffForHumans() }}.</h4>
-            <div class="whitespace-pre-line py-2 text-base text-black-500">{{ $posts->content }}</div>
-            <livewire:upvote :likeableType="'App\\Models\\Post'" :likeableId="$posts->id" :wire:key="'post-like-' . $posts->id"/>
-          </td>
-        </tr>  
+<div class="container mx-auto max-w-3xl py-2">
+  @foreach (\App\Models\Post::all()->sortBy('created_at')->reverse() as $post)
+  <div class="card-body flex-auto">
+		<div class="px-5 py-5 rounded-xl shadow-xl bg-white" data-post-id="{{ $post->id }}">
+    <div>
+      <div class="flex items-center">
+        <img class="w-12 h-12 rounded-full mr-4 max-w-4xl" src="{{ $post->user->profile_picture}}">
+        <a class="font-bold text-lg" href="{{ route('profile', $post->user->user_name) }}">{{ $post->user->first_name }} {{ $post->user->last_name }}</a> ‎ @‎{{$post->user->user_name}} · @if($post->created_at->diffInDays(now()) < 2) {{ $post->created_at->diffForHumans() }} @else {{$post->created_at->toFormattedDateString()}} @endif
+      </div>
+      <p class="card-text text-base py-4 break-normal">{{ $post->content }}</p>
+      <div class="flex items-center">
+				<livewire:upvote :likeableType="'App\\Models\\Post'" :likeableId="$post->id" :wire:key="'post-like-' . $post->id" />
+			</div> 
+			@if(Auth::check() && Auth::user()->id == Auth::user()->id) 
+				<div class="flex items-center my-4 ">
+					<livewire:post-delete :postId="$post->id" :wire:key="'post-delete-' . $post->id" />
+				</div> 
+			@endif
+			</div>
+    </div>
+  </div>  
   @endforeach
-  </table>
+  </div>
 </div>
