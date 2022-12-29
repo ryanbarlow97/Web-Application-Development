dcb6f3d4cf53681d4269d7e18e5c6c4ed85d8f23
Ryan Barlow <908032@swansea.ac.uk>
2022-12-29 21:15:43 +0000

    reworked classes to look like post/homepage

--- resources/views/livewire/show-comments.blade.php
+++ resources/views/livewire/show-comments.blade.php
@@ -1,35 +1,33 @@
 <div>
 	<!-- Use the list-group class to style the comments -->
-	<ul class="card mb-3">
+	<ul class="card">
 		@foreach ($post->comments()->orderBy('created_at', 'asc')->get() as $comment)
 		<!-- Use the list-group-item class to style each comment -->
-		<li class="card-body p-6">
+		<li class="card-body pt-2">
 			<!-- Use the media class to style the comment avatar and content -->
-			<div class="px-5 flex items-center mb-4 rounded-xl shadow-lg" data-post-id="{{ $post->id }}">
+			<div class="px-5 py-5 flex items-center rounded-xl shadow-xl bg-white" data-comment-id="{{ $comment->id }}">
 				<div>
 					<!-- Use the mb-0 class to remove the bottom margin from the comment username -->
-					<h5 class="text-lg mb-0">
-						<div class="font-bold flex items-center">
+					<h5>
+						<div class="flex items-center">
 						<img class="w-12 h-12 rounded-full mr-4 max-w-4xl" src="{{ $comment->user->profile_picture}}">
-						<a href="{{ route('profile', $comment->user->user_name) }}">{{ $comment->user->first_name }} {{ $comment->user->last_name }}</a>
+						<a class="font-bold text-lg" href="{{ route('profile', $comment->user->user_name) }}">{{ $comment->user->first_name }} {{ $comment->user->last_name }}</a>  ‎ @‎{{$comment->user->user_name}} · @if($comment->created_at->diffInDays(now()) < 2) {{ $comment->created_at->diffForHumans() }} @else {{$comment->created_at->toFormattedDateString()}} @endif
 						</div>
 						<!-- Use the small class to style the comment timestamp -->
-						<small class="text-gray-700">{{ $comment->created_at->diffForHumans() }}</small>
 					</h5>
 					<!-- Use the card-text class to style the comment content -->
-					<p class="card-text text-base py-4 break-words">{{ $comment->content }}</p>
+					<p class="card-text pt-2 text-base break-words">{{ $comment->content }}</p>
 					<div>
-						<div class="flex items-center py-2 mb-4">
-							<livewire:upvote :likeableType="'App\\Models\\Comment'" :likeableId="$comment->id" :wire:key="'comment-like-' . $comment->id"/>
+						<div class="items-center">
+							<livewire:upvote :likeableType="'App\\Models\\Comment'" :likeableId="$comment->id" :wire:key="'comment-like-' . $comment->id" />
 						</div>
 						@if(Auth::check() && Auth::user()->id == $comment->user_id)
-						<div class="flex items-center mb-4">
+						<div class="items-center">
 							<livewire:comment-delete :commentId="$comment->id" :wire:key="'comment-delete-' . $comment->id"/>
 						</div>
 						@endif
 					</div>
 				</div>
-
 			</div>
 		</li>
 		@endforeach
