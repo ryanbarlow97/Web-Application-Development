6ba45e17821e3c88a33f990e4e635e9432776f27
Ryan Barlow <908032@swansea.ac.uk>
2022-12-29 21:15:09 +0000

    changed how comment box renders, looks much nicer now

--- resources/views/livewire/comment-create.blade.php
+++ resources/views/livewire/comment-create.blade.php
@@ -1,16 +1,11 @@
-<!-- Use the container class to center the content -->
-<div class="container">
-  <!-- Use the form-group class to style the form group -->
-  <form wire:submit.prevent="store" class="mb-6">
-    <!-- Use the flex class to align the elements horizontally -->
-    <div class="flex items-center rounded-md">
-      <!-- Use the form-control class to style the comment input -->
-      @csrf
-      <!-- Use the btn and btn-primary classes to style the comment button -->
-      <div>
-        <input wire:model.lazy="content" name="content" type="text" placeholder="Add a comment..." required class="form-control rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 py-2 px-4">
-        <button type="submit" class="btn btn-primary rounded-md shadow-sm py-2 px-4 bg-blue-500 hover:bg-blue-700 active:bg-blue-700 focus:outline-none">Comment</button>
-      </div>
-    </div>
-  </form>
+<div>
+  <div class="mx-auto items-center rounded-b-xl px-5 py-4 shadow-xl bg-white">
+    <form wire:submit.prevent="store">
+        @csrf
+        <div class="flex justify-between rounded-md shadow-sm">
+            <input wire:model.lazy="content" name="content" type="text" placeholder="Add a comment..." required class="form-control rounded-md shadow-sm flex-1 focus:outline-none focus:ring-blue-700 focus:border-blue-700 w-full py-2 px-4 mr-2">
+            <button type="submit" class="btn btn-primary rounded-md shadow-sm py-2 px-4 bg-blue-500 hover:bg-blue-700 active:bg-blue-700 focus:outline-none">Comment</button>
+        </div>
+      </form>
+  </div>
 </div>
