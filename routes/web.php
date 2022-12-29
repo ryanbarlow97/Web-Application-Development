04c9797340c107936fb38e477bee13d785ef5ab6
Ryan Barlow <908032@swansea.ac.uk>
2022-12-29 14:14:18 +0000

    remove uneeded routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\UpvoteController;
-use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,31 +26,13 @@ Route::get('/notifications', function () {
     return view('notifications');
 })->middleware(['auth', 'verified'])->name('notifications');
 
-//Profile Posts
-Route::get('/users/{id}/posts', [UserController::class, 'posts'])
-    ->name('users.posts');
-//Profile Comments
-Route::get('/users/{id}/comments', [UserController::class, 'comments'])
-    ->name('users.comments');
-//Profile Liked Posts
-Route::get('/users/{id}/liked_posts', [UserController::class, 'likedPosts'])
-    ->name('users.liked_posts');
-//Profile Liked Comments
-Route::get('/users/{id}/liked_comments', [UserController::class, 'likedComments'])
-    ->name('users.liked_comments');
-
-//View User ID
-Route::get('/users/{id}', [UserController::class, 'show'])
-->name('users.show');
-
+//View Profile ID
+Route::get('/profile/{id}', App\Http\Livewire\ShowProfile::class)
+    ->middleware(['auth', 'verified'])->name('profile');
 
 //View Post ID
-Route::get('/posts/{id}', App\Http\Livewire\ShowPost::class);
-
-//Create Post
-
-//Create Comment
-Route::get('/upvote', App\Http\Livewire\Upvote::class);
+Route::get('/comments/{id}', App\Http\Livewire\ShowPost::class)
+    ->middleware(['auth', 'verified'])->name('post');
 
 
 require __DIR__.'/auth.php';
