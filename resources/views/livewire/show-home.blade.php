f281420f04c44bed27660d1827747d65350cbb29
Ryan Barlow <908032@swansea.ac.uk>
2022-12-29 21:23:03 +0000

    attempt to fix like and delete

--- resources/views/livewire/show-home.blade.php
+++ resources/views/livewire/show-home.blade.php
@@ -12,22 +12,23 @@
 
 <div class="container mx-auto max-w-3xl py-2">
   @foreach (\App\Models\Post::all()->sortBy('created_at')->reverse() as $post)
-  <div class="card-body flex-auto">
+  <div class="card-body flex-auto py-2">
 		<div class="px-5 py-5 rounded-xl shadow-xl bg-white" data-post-id="{{ $post->id }}">
     <div>
       <div class="flex items-center">
         <img class="w-12 h-12 rounded-full mr-4 max-w-4xl" src="{{ $post->user->profile_picture}}">
-        <a class="font-bold text-lg" href="{{ route('profile', $post->user->user_name) }}">{{ $post->user->first_name }} {{ $post->user->last_name }}</a> ‎ @‎{{$post->user->user_name}} · @if($post->created_at->diffInDays(now()) < 2) {{ $post->created_at->diffForHumans() }} @else {{$post->created_at->toFormattedDateString()}} @endif
+        <a class="font-bold text-lg hover:text-blue-700 active:text-blue-700" href="{{ route('profile', $post->user->user_name) }}">{{ $post->user->first_name }} {{ $post->user->last_name }}</a> ‎ @‎{{$post->user->user_name}} · @if($post->created_at->diffInDays(now()) < 2) {{ $post->created_at->diffForHumans() }} @else {{$post->created_at->toFormattedDateString()}} @endif
       </div>
       <p class="card-text text-base py-4 break-normal">{{ $post->content }}</p>
-      <div class="flex items-center">
-				<livewire:upvote :likeableType="'App\\Models\\Post'" :likeableId="$post->id" :wire:key="'post-like-' . $post->id" />
-			</div> 
-			@if(Auth::check() && Auth::user()->id == Auth::user()->id) 
-				<div class="flex items-center my-4 ">
-					<livewire:post-delete :postId="$post->id" :wire:key="'post-delete-' . $post->id" />
-				</div> 
-			@endif
+      <div>
+				<div class="items-center">
+					<livewire:upvote :likeableType="'App\\Models\\Post'" :likeableId="$post->id" :wire:key="'post-like-' . $post->id" />
+				</div>
+				@if(Auth::check() && Auth::user()->id == Auth::user()->id)
+				<div class="items-center">
+					<livewire:post-delete :postId="$post->id" :wire:key="'post-delete-' . $post->id"/>
+				</div>
+				@endif
 			</div>
     </div>
   </div>  
