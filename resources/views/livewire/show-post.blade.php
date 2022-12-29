028668d6d1f4b447c2d44571a60bf8141f3cd63a
Ryan Barlow <908032@swansea.ac.uk>
2022-12-29 21:14:42 +0000

    reworked all classesto look nicer

--- resources/views/livewire/show-post.blade.php
+++ resources/views/livewire/show-post.blade.php
@@ -1,39 +1,32 @@
 <div>
 <!-- Include the link to the stylesheet from the CDN -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css">
-<!-- Add the Livewire scripts --> 
-@livewireScripts
+<!-- Add the Livewire scripts --> @livewireScripts
 <!-- Use the container class to center the content -->
-<div class="container mx-auto max-w-3xl">
-	<!-- Use the card class to style the post -->
-	<div class="card mb-3">
-		<div class="card-body p-6">
+<div class="container mx-auto max-w-3xl py-2">
+		<div class="card-body">
 			<!-- Use the flex class to align the elements horizontally -->
-			<div class="px-5 flex items-center mb-4 rounded-xl shadow-xl" data-post-id="{{ $post->id }}">
-                <div>
-                <h5 class="text-lg mb-0">
-						<div class="font-bold flex items-center">
+			<div class="px-5 pt-5 flex rounded-t-xl shadow-xl bg-white" data-post-id="{{ $post->id }}">
+				<div>
+					<div class="flex items-center">
 						<img class="w-12 h-12 rounded-full mr-4 max-w-4xl" src="{{ $post->user->profile_picture}}">
-						<a href="{{ route('profile', $post->user->user_name) }}">{{ $post->user->first_name }} {{ $post->user->last_name }}</a>
-						</div>
-						<!-- Use the small class to style the comment timestamp -->
-						<small class="text-gray-700">{{ $post->created_at->diffForHumans() }}</small>
-					</h5>
-
-					<!-- Use the card-text class to style the post content -->
-					<p class="card-text text-base py-4 break-normal">{{ $post->content }}</p>
-					<div class="flex items-center mb-4">
-						<livewire:upvote :likeableType="'App\\Models\\Post'" :likeableId="$post->id" />
+						<a class="text-lg font-bold" href="{{ route('profile', $post->user->user_name) }}">{{ $post->user->first_name }} {{ $post->user->last_name }}</a> ‎ @‎{{$post->user->user_name}}  · @if($post->created_at->diffInDays(now()) < 2) {{ $post->created_at->diffForHumans() }} @else {{$post->created_at->toFormattedDateString()}} @endif
 					</div>
-                    <livewire:comment-create :post_id="$post->id" />
-					@if(Auth::check() && Auth::user()->id == Auth::user()->id)
-						<div class="flex items-center mb-4">
-							<livewire:post-delete :postId="$post->id" :wire:key="'post-delete-' . $post->id"/>
-						</div>
-						@endif
-				</div>  
-			</div>     
+					<!-- Use the card-text class to style the post content -->
+					<p class="card-text pt-2 text-base break-normal">{{ $post->content }}</p>
+					<div class="flex items-center">
+						<livewire:upvote :likeableType="'App\\Models\\Post'" :likeableId="$post->id" :wire:key="'post-like-' . $post->id" />
+					</div> 
+					@if(Auth::check() && Auth::user()->id == $post->user->id) 
+					<div class="flex items-center my-4 ">
+					<livewire:post-edit :postId="$post->id" :wire:key="'post-edit-' . $post->id" />
+					<livewire:post-delete :postId="$post->id" :wire:key="'post-delete-' . $post->id" />
+					</div> 
+					@endif
+				</div>
+			</div>
+			<livewire:comment-create :post_id="$post->id" />
 		</div>
-        <livewire:show-comments :post="$post" />
+		<livewire:show-comments :post="$post" />
 	</div>
 </div>
