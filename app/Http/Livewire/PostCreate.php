31bbc8671da1fe1e140f2c60838956c7c88d8dcc
Ryan Barlow <908032@swansea.ac.uk>
2022-12-30 03:16:53 +0000

    attempt to upload images (not working yet)

--- app/Http/Livewire/PostCreate.php
+++ app/Http/Livewire/PostCreate.php
@@ -3,46 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use Livewire\WithFileUploads;
-use Illuminate\Support\Facades\Storage;
-use Ramsey\Uuid\Uuid;
+use App\Models\Post;
 
 class PostCreate extends Component
 {
-    use WithFileUploads;
-
     public $content;
-    public $photoUrl;
-    public $photo;
 
-
-    public function fileChanged()
-    {
-        $filename = Uuid::uuid4()->toString();
-        $filename = $filename . '.jpg';
-
-        $this->photoUrl = Storage::disk('public/posts')->url($filename);
-    }
-    
     public function store()
     {
         $validatedData = $this->validate([
             'content' => 'required',
-            'photo' => 'image|mimes:jpeg,png,jpg|max:2048',
-            'photoUrl' => 'string|max:1024',
         ]);
 
         $post = Post::create([
+            'user_id' => auth()->user()->id,
             'content' => $this->content,
-            ''
-
+            'flair'   => "demo",
         ]);
 
         $this->reset(['content']);
-    }
 
+        $this->emit('newPost');
+
+
+    }
     public function render()
     {
         return view('livewire.post-create');
-    }
+    }    
 }
