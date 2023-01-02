8086ee8b82d8721f89fc3889c2fc3c81620d406e
Ryan Barlow <908032@swansea.ac.uk>
2023-01-02 15:16:22 +0000

    added polling, and changed posts to use new inline method from comments

--- resources/views/livewire/show-post.blade.php
+++ resources/views/livewire/show-post.blade.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css">
 <!-- Add the Livewire scripts --> @livewireScripts
 <!-- Use the container class to center the content -->
-	<div class="container mx-auto max-w-3xl py-2">
+	<div class="container mx-auto max-w-3xl py-2" wire:poll.2s>
 		<div class="card-body py-1 w-full rounded-xl text-left">
 			<!-- Use the flex class to align the elements horizontally -->
 			<div class="px-5 pt-5 flex rounded-t-xl bg-white shadow-xl" data-post-id="{{ $post->id }}">
@@ -12,8 +12,16 @@
 						<img class="w-12 h-12 rounded-full mr-4 max-w-4xl" src="{{ $post->user->profile_picture}}">
 						<a class="text-lg font-bold hover:text-blue-700 active:text-blue-700" href="{{ route('profile', $post->user->user_name) }}">{{ $post->user->first_name }} {{ $post->user->last_name }}</a> ‎ @‎{{$post->user->user_name}}  · @if($post->created_at->diffInDays(now()) < 2) {{ $post->created_at->diffForHumans() }} @else {{$post->created_at->toFormattedDateString()}} @endif
 					</div>
-					<!-- Use the card-text class to style the post content -->
+
+					@if(Auth::check() && Auth::user()->id == $post->user_id)
+						<div class="items-center">
+							@livewire('post-edit', compact('post'), key($post->id))
+						</div>
+					@endif
+
+					@if(Auth::check() && Auth::user()->id != $post->user_id)
 					<p class="card-text py-2 text-base break-normal" data-post-edit-id="{{'post-edit-' . $post->id }}">{{ $post->content }}</p>
+					@endif
 
 					@if($post->image != null)
 					<img class="flex mx-auto rounded-xl mb-2 shadow-xl" src="{{asset('storage/'.$post->image->path)}}">
@@ -26,7 +34,6 @@
 					@endif
 					@if(Auth::check() && Auth::user()->id == $post->user->id) 
 					<div class="flex">
-						<livewire:post-edit :postId="$post->id" :wire:key="'post-edit-' . $post->id" />
 						<livewire:post-delete :postId="$post->id" :wire:key="'post-delete-' . $post->id" />
 					</div> 
 					@endif
