c901ffaae6705ec01f24f2b0766e75d4bcc54ccb
Ryan Barlow <908032@swansea.ac.uk>
2023-01-05 01:07:38 +0000

    realised the thumbnail doesn't always exist, so added a check to see if the thumbnail exists

--- app/Http/Controllers/TwitchController.php
+++ app/Http/Controllers/TwitchController.php
@@ -9,11 +9,15 @@ class TwitchController extends Controller
 {
     public function create(Post $post, Twitch $twitch)
     {
-        $position = rand(0,9);
+        $position = rand(0,19);
         $clip = $twitch->getTopStream($position);
 
         $string = $clip->thumbnail_url;
-        if (strpos($string, '%{width}x%{height}') !== false) {
+
+        if ( $string == 'https://vod-secure.twitch.tv/_404/404_processing_%{width}x%{height}.png') {
+            $string = 'https://vod-secure.twitch.tv/_404/404_processing_320x180.png';
+        }
+        else if (strpos($string, '%{width}x%{height}') !== false) {
             $string = str_replace('%{width}x%{height}', '1920x1080', $string);
         }
 
