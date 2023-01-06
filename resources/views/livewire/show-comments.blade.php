9e8234d4c1e5e1e1cb5b15b8300b88d1b6472807
Ryan Barlow <908032@swansea.ac.uk>
2023-01-06 20:34:50 +0000

    removed outdated comments

--- resources/views/livewire/show-comments.blade.php
+++ resources/views/livewire/show-comments.blade.php
@@ -1,21 +1,15 @@
 <div>
-	<!-- Use the list-group class to style the comments -->
 	<ul class="card" wire:poll.visible>
 		@foreach ($post->comments()->orderBy('created_at', 'asc')->get() as $comment)
-		<!-- Use the list-group-item class to style each comment -->
 		<li class="card-body pt-2">
-			<!-- Use the media class to style the comment avatar and content -->
 			<div class="px-5 py-5 flex items-center rounded-xl shadow-xl bg-white" data-comment-id="{{ $comment->id }}">
 				<div>
-					<!-- Use the mb-0 class to remove the bottom margin from the comment username -->
 					<h5>
 						<div class="flex items-center">
 						<img class="w-12 h-12 rounded-full mr-4 max-w-4xl" src="{{ asset('/storage/'.$comment->user->profile_picture)}}">
 						<a class="font-bold text-lg hover:text-blue-700 active:text-blue-700" href="{{ route('profile', $comment->user->user_name) }}">{{ $comment->user->first_name }} {{ $comment->user->last_name }}</a>  ‎ @‎{{$comment->user->user_name}} · @if($comment->created_at->diffInDays(now()) < 2) {{ $comment->created_at->diffForHumans() }} @else {{$comment->created_at->toFormattedDateString()}} @endif
 						</div>
-						<!-- Use the small class to style the comment timestamp -->
 					</h5>
-					<!-- Use the card-text class to style the comment content -->
 					@if(Auth::check() && (Auth::user()->id == $comment->user_id || Auth::user()->priority == 1))
 						<div class="items-center">
 							@livewire('comment-edit', compact('comment'), key($comment->id))
